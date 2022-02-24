fdebbdcb972d94bb8e9d7c393513e54158402e83
Sreedhar <sreedhar.kokkarachedu@outlook.de>
2022-02-24 14:13:52 +0100

    deleting_Unnecessary_code

--- skip_elements.py
+++ skip_elements.py
@@ -14,24 +14,6 @@ def skip_elements(elements):
 
 	return new_list
 
-def skip_elements(elements):
-	# Initialize variables
-	new_list = []
-	i = 0
-
-	# Iterate through the list
-	for x in elements:
-		# Does this element belong in the resulting list?
-		if i%2==0:
-			# Add this element to the resulting list
-			new_list.append(x)
-		# Increment i
-		i+=1
-
-	return new_list
-
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
-print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
-print(skip_elements([])) # Should be []
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
 print(skip_elements([])) # Should be []
